65d43ec5c8bbcd42348d6528f17b97d8bf2a7995
chuckie82 <chuckie82@gmail.com>
2017-10-24 22:27:15 -0700

    added host dark subtraction time

--- profileBlockSize/profileBlockSize.cu
+++ profileBlockSize/profileBlockSize.cu
@@ -1,5 +1,11 @@
 #include <stdio.h>
 #include <cuda_profiler_api.h>
+#include <unistd.h>
+
+#include <sys/time.h>
+#include <iostream>
+#include <iomanip>
+using namespace std;
 
 // Convenience function for checking CUDA runtime API results
 // can be wrapped around any runtime API call. No-op in release builds.
@@ -41,46 +47,66 @@ float maxError(short *a, int n)
 
 int main(int argc, char **argv)
 {
-  const int nPixels = 2400000;
-  const int nEvents = atoi(argv[1]);
-  const int n = nPixels * nEvents;
+  const int nPixels = 2400000;				// no. of pixels per image
+  const int nEvents = atoi(argv[1]);			// no. of events
+  const int n = nPixels * nEvents;			// total number of pixels
   
-  const int nStreams = atoi(argv[2]);
-  const int streamSize = n / nStreams;
+  const int nStreams = atoi(argv[2]);			// no. of stream
+  const int streamSize = n / nStreams;			// stream size (pixels)
 
-  const int streamBytes = streamSize * sizeof(short);
-  const int bytes = n * sizeof(short);
-  const int darkBytes = nPixels * sizeof(short);
+  const int streamBytes = streamSize * sizeof(short);	// stream size (bytes)
+  const int bytes = n * sizeof(short);			// total size (bytes)
+  const int darkBytes = nPixels * sizeof(short);	// dark size (bytes)
 
   // max block size is 1024
-  const int blockSize = atoi(argv[3]);
+  const int blockSize = atoi(argv[3]);			// block size
   printf("Running with nStreams: %d streamSize: %d\n", nStreams, streamSize);
-  int gridSize = streamSize / blockSize;
+  int gridSize = streamSize / blockSize;		// grid size
   printf("blockSize: %d gridSize: %d\n", blockSize, gridSize);
 
   int devId = 0;
-  if (argc > 4) devId = atoi(argv[4]);
+  if (argc > 4) devId = atoi(argv[4]);			// device ID (optional)
   
+  // print device name
   cudaDeviceProp prop;
   checkCuda( cudaGetDeviceProperties(&prop, devId));
   printf("Device : %s\n", prop.name);
   checkCuda( cudaSetDevice(devId) );
 
   // allocate pinned host memory and device memory
-  short *a, *d_a; // data
-  checkCuda( cudaMallocHost((void**)&a, bytes) ); // host pinned
-  checkCuda( cudaMalloc((void**)&d_a, bytes) ); // device  
-  short *dark, *d_dark; // dark
-  checkCuda( cudaMallocHost((void**)&dark, darkBytes) ); 
-  checkCuda( cudaMalloc((void**)&d_dark, darkBytes) ); 
+  short *a, *d_a; 						// data address
+  checkCuda( cudaMallocHost((void**)&a, bytes) ); 		// host pinned
+  checkCuda( cudaMalloc((void**)&d_a, bytes) ); 		// device  
+  short *dark, *d_dark;					 	// dark address
+  checkCuda( cudaMallocHost((void**)&dark, darkBytes) ); 	// host pinned
+  checkCuda( cudaMalloc((void**)&d_dark, darkBytes) );		// device
   
-  // prepare raw and dark data
+  // prepare data (all 1's) and dark (all 0's) on host
   fill(a, n, 1);
   fill(dark, nPixels, 0);
   printf("Input values (Data): %d %d %d...%d %d %d\n", a[0], a[1], a[2], a[n-3], a[n-2], a[n-1]);
   printf("Input values (Dark): %d %d %d...%d %d %d\n", dark[0], dark[1], dark[2], dark[nPixels-3], dark[nPixels-2], dark[nPixels-1]);
 
-  // serial copy for one dark 
+  // host calculation
+    struct timeval start, end;
+
+    long seconds, useconds;    
+    double mtime;
+
+    gettimeofday(&start, NULL);
+
+    for(int i=0; i<nPixels; i++)
+      a[i] -= dark[i];
+
+    gettimeofday(&end, NULL);
+
+    seconds  = end.tv_sec  - start.tv_sec;
+    useconds = end.tv_usec - start.tv_usec;
+    mtime = ((seconds) * 1000000 + useconds)/1000.0;// + 0.5;
+
+    cout << "Host dark-subtraction took "<< mtime <<" ms for 1 event."<< endl;
+
+  // serial copy for one dark to device 
   checkCuda( cudaMemcpy(d_dark, dark, darkBytes, cudaMemcpyHostToDevice) );
 
   float ms; // elapsed time in milliseconds
@@ -98,13 +124,13 @@ int main(int argc, char **argv)
   checkCuda( cudaEventRecord(startEvent, 0) );
   cudaProfilerStart();
   for (int i = 0; i < nStreams; ++i) {
-    int offset = i * streamSize;
+    int offset = i * streamSize;				// offset by no. of pixels in stream
     int offsetDark = offset % nPixels;
-    checkCuda( cudaMemcpyAsync(&d_a[offset], &a[offset],
+    checkCuda( cudaMemcpyAsync(&d_a[offset], &a[offset],	// async copy stream from host to device
                                streamBytes, cudaMemcpyHostToDevice,
                                stream[i]) );
-    kernel<<<gridSize, blockSize, 0, stream[i]>>>(d_a, offset, d_dark, offsetDark);
-    checkCuda( cudaMemcpyAsync(&a[offset], &d_a[offset],
+    kernel<<<gridSize, blockSize, 0, stream[i]>>>(d_a, offset, d_dark, offsetDark); // subtract dark
+    checkCuda( cudaMemcpyAsync(&a[offset], &d_a[offset],	// async copy stream from device to host
                                streamBytes, cudaMemcpyDeviceToHost,
                                stream[i]) );
   }
