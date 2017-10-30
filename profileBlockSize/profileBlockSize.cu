2bbdd3a69889594f674ee9974d729a4e62fc71fe
monarin <monarin@gmail.com>
2017-10-30 12:16:19 -0700

    nStreams and blockSize can be any integer now.

--- profileBlockSize/profileBlockSize.cu
+++ profileBlockSize/profileBlockSize.cu
@@ -26,31 +26,42 @@ cudaError_t checkCuda(cudaError_t result)
   return result;
 }
 
-__global__ void kernel(short *a, int offset, short *dark)
+__global__ void kernel(float *a, int offset, float *dark, int streamSize)
 {
-  int i = offset + threadIdx.x + blockIdx.x*blockDim.x;
-  int iDark = i % N_PIXELS;
-  a[i] -= dark[iDark];
+  int idx = threadIdx.x + blockIdx.x*blockDim.x; // thread id
+  
+  // check if idx < streamSize
+  if (idx < streamSize) {
+    int iData = offset + idx;
+    int iDark = iData % N_PIXELS;
+    a[iData] -= dark[iDark];
+  }
 }
 
 /* ---------------------- host code -----------------------------*/
-void fill( short *p, int n, int val ) {
+void fill( float *p, int n, float val ) {
   for(int i = 0; i < n; i++){
     p[i] = val;
   }
 }
 
-float maxError(short *aCalc, short *aKnown, int n)
+float maxError(float *aCalc, float *aKnown, int nEvents, int nPixels)
 {
   float maxE = 0;
-  for (int i = 0; i < n; i++) {
-    float error = fabs(aCalc[i]-aKnown[i]);
-    if (error > maxE) maxE = error;
+  for (int i = 0; i < nEvents; i++) {
+    int offset = i * nPixels;
+    for (int j = 0; j < nPixels; j++) {
+      int idx = offset + j;
+      float error = fabs(aCalc[idx]-aKnown[j]);
+      if (error > 1.0)
+      printf("offset: %d j: %d idx: %d error %e aCalc[idx]: %8.2f aKnown[j]: %8.2f\n", offset, j, idx, error, aCalc[idx], aKnown[j]);
+      if (error > maxE) maxE = error;
+    }
   }
   return maxE;
 }
 
-void host_calc(short *a, short *dark, int n) {
+void host_calc(float *a, float *dark, int nPixels) {
   // host calculation
   struct timeval start, end;
 
@@ -59,7 +70,7 @@ void host_calc(short *a, short *dark, int n) {
 
   gettimeofday(&start, NULL);
 
-  for(int i=0; i<n; i++)
+  for(int i=0; i < nPixels; i++)
     a[i] -= dark[i];
 
   gettimeofday(&end, NULL);
@@ -73,23 +84,16 @@ void host_calc(short *a, short *dark, int n) {
 
 int main(int argc, char **argv)
 {
-  const int nPixels = 2296960;			// no. of pixels per image
+  const int nPixels = N_PIXELS;			// no. of pixels per image
   const int nEvents = atoi(argv[1]);			// no. of events
   const int n = nPixels * nEvents;			// total number of pixels
   
   const int nStreams = atoi(argv[2]);			// no. of stream
-  const int streamSize = n / nStreams;			// stream size (pixels)
+  const int blockSize = atoi(argv[3]);                  // block size (max is 1024)
 
-  const int streamBytes = streamSize * sizeof(short);	// stream size (bytes)
-  const int bytes = n * sizeof(short);			// total size (bytes)
-  const int darkBytes = nPixels * sizeof(short);	// dark size (bytes)
-
-  // max block size is 1024
-  const int blockSize = atoi(argv[3]);			// block size
-  printf("Running with nStreams: %d streamSize: %d\n", nStreams, streamSize);
-  int gridSize = streamSize / blockSize;		// grid size
-  printf("blockSize: %d gridSize: %d\n", blockSize, gridSize);
-
+  const int bytes = n * sizeof(float);			// total size (bytes)
+  const int darkBytes = nPixels * sizeof(float);	// dark size (bytes)
+  
   int devId = 0;
   if (argc > 4) devId = atoi(argv[4]);			// device ID (optional)
   
@@ -100,27 +104,30 @@ int main(int argc, char **argv)
   checkCuda( cudaSetDevice(devId) );
 
   // allocate pinned host memory and device memory
-  // RAW
-  short *a, *d_a; 						// data address
+  // RAW * nEVents
+  float *a, *d_a; 						// data address
   checkCuda( cudaMallocHost((void**)&a, bytes) ); 		// host pinned
   checkCuda( cudaMalloc((void**)&d_a, bytes) ); 		// device
+  // SINGLE RAW
+  float *raw;                                               // data address
+  checkCuda( cudaMallocHost((void**)&raw, darkBytes) );     // host pinned
   // RAW-PEDESTAL
-  short *pedCorrected, *d_pedCorrected; 			// data address
+  float *pedCorrected, *d_pedCorrected; 			// data address
   checkCuda( cudaMallocHost((void**)&pedCorrected, darkBytes) ); // host pinned
   checkCuda( cudaMalloc((void**)&d_pedCorrected, darkBytes) ); 	// device  
   // PEDESTAL
-  short *dark, *d_dark;					 	// dark address
+  float *dark, *d_dark;					 	// dark address
   checkCuda( cudaMallocHost((void**)&dark, darkBytes) ); 	// host pinned
   checkCuda( cudaMalloc((void**)&d_dark, darkBytes) );		// device
   // PER-PIXEL GAIN
-  short *gain, *d_gain;					 	// dark address
+  float *gain, *d_gain;					 	// dark address
   checkCuda( cudaMallocHost((void**)&gain, darkBytes) ); 	// host pinned
   checkCuda( cudaMalloc((void**)&d_gain, darkBytes) );		// device
   // RAW-PEDESTAL
-  short *calib, *d_calib;					 	// dark address
+  float *calib, *d_calib;					 	// dark address
   checkCuda( cudaMallocHost((void**)&calib, darkBytes) ); 	// host pinned
   checkCuda( cudaMalloc((void**)&d_calib, darkBytes) );		// device  
-  // prepare data (all 1's) and dark (all 0's) on host
+  
   //load the text file and put it into a single string:
   ifstream inR("/reg/data/ana14/cxi/cxitut13/res/yoon82/psgpu/profileBlockSize/cxid9114_raw_95.txt");
   ifstream inPC("/reg/data/ana14/cxi/cxitut13/res/yoon82/psgpu/profileBlockSize/cxid9114_pedCorrected_95.txt");
@@ -129,9 +136,14 @@ int main(int argc, char **argv)
   ifstream inC("/reg/data/ana14/cxi/cxitut13/res/yoon82/psgpu/profileBlockSize/cxid9114_calib_95.txt");
   // Fill arrays from text files
   string line;
-  for (unsigned int i=0; i<nPixels;i++){
+  for (unsigned int i=0; i<nPixels; i++){
     getline(inR, line);
-    a[i] = atof(line.c_str());
+    raw[i] = atof(line.c_str());
+    //populate all events with the same set of test data
+    for (int j=0; j<nEvents; j++) {
+      int offset = j * nPixels;
+      a[offset + i] = raw[i];
+    }
     getline(inPC, line);
     pedCorrected[i] = atof(line.c_str());
     getline(inP, line);
@@ -142,22 +154,15 @@ int main(int argc, char **argv)
     calib[i] = atof(line.c_str());
   }
 
-  printf("Input values (Data): %d %d %d...%d %d %d\n", a[0], a[1], a[2], a[n-3], a[n-2], a[n-1]);
-  printf("Input values (Dark): %d %d %d...%d %d %d\n", dark[0], dark[1], dark[2], dark[nPixels-3], dark[nPixels-2], dark[nPixels-1]);
+  printf("Input values (Data): %8.2f %8.2f %8.2f...%8.2f %8.2f %8.2f\n", a[0], a[1], a[2], a[n-3], a[n-2], a[n-1]);
+  printf("Input values (Dark): %8.2f %8.2f %8.2f...%8.2f %8.2f %8.2f\n", dark[0], dark[1], dark[2], dark[nPixels-3], dark[nPixels-2], dark[nPixels-1]);
 
   // host calculation 
-  host_calc(a, dark, nPixels);
+  host_calc(raw, dark, nPixels);
   printf("Host Calculation\n");
-  printf("Input values (Data calc.): %d %d %d...%d %d %d\n", a[0], a[1], a[2], a[n-3], a[n-2], a[n-1]);
-  printf("Input values (Data known): %d %d %d...%d %d %d\n", pedCorrected[0], pedCorrected[1], pedCorrected[2], pedCorrected[nPixels-3], pedCorrected[nPixels-2], pedCorrected[nPixels-1]);
-  printf("  max error: %e\n", maxError(a, pedCorrected, n));
-
-  // reload data
-  ifstream inR2("/reg/data/ana14/cxi/cxitut13/res/yoon82/psgpu/profileBlockSize/cxid9114_raw_95.txt");
-  for (unsigned int i=0; i<nPixels;i++){
-    getline(inR2, line);
-    a[i] = atof(line.c_str());
-  }
+  printf("Input values (Data calc.): %8.2f %8.2f %8.2f...%8.2f %8.2f %8.2f\n", raw[0], raw[1], raw[2], raw[nPixels-3], raw[nPixels-2], raw[nPixels-1]);
+  printf("Input values (Data known): %8.2f %8.2f %8.2f...%8.2f %8.2f %8.2f\n", pedCorrected[0], pedCorrected[1], pedCorrected[2], pedCorrected[nPixels-3], pedCorrected[nPixels-2], pedCorrected[nPixels-1]);
+  printf("  max error: %e\n", maxError(raw, pedCorrected, 1, nPixels));
 
   // serial copy for one dark to device 
   checkCuda( cudaMemcpy(d_dark, dark, darkBytes, cudaMemcpyHostToDevice) );
@@ -177,11 +182,21 @@ int main(int argc, char **argv)
   checkCuda( cudaEventRecord(startEvent, 0) );
   cudaProfilerStart();
   for (int i = 0; i < nStreams; ++i) {
+    int streamSize = ceil( (double) n / nStreams );  // stream size (pixels)
     int offset = i * streamSize;
+
+    // check if last stream has full length
+    if ( (i + 1) * streamSize > n ) streamSize = n - (i * streamSize);
+
+    int streamBytes = streamSize * sizeof(float);   // stream size (bytes)
+    //printf("Stream#: %d streamSize: %d offset=%d\n", i, streamSize, offset);
+    int gridSize = ceil(  (double) streamSize / blockSize );               // grid size
+    //printf("blockSize: %d gridSize: %d\n", blockSize, gridSize);
+
     checkCuda( cudaMemcpyAsync(&d_a[offset], &a[offset],
                                streamBytes, cudaMemcpyHostToDevice,
                                stream[i]) );
-    kernel<<<gridSize, blockSize, 0, stream[i]>>>(d_a, offset, d_dark);
+    kernel<<<gridSize, blockSize, 0, stream[i]>>>(d_a, offset, d_dark, streamSize);
     checkCuda( cudaMemcpyAsync(&a[offset], &d_a[offset],
                                streamBytes, cudaMemcpyDeviceToHost,
                                stream[i]) );
@@ -192,9 +207,9 @@ int main(int argc, char **argv)
   checkCuda( cudaEventElapsedTime(&ms, startEvent, stopEvent) );
   printf("GPU Calculation\n");
   printf("Time for asynchronous V1 transfer and execute (ms): %f\n", ms);
-  printf("Input values (Data calc.): %d %d %d...%d %d %d\n", a[0], a[1], a[2], a[n-3], a[n-2], a[n-1]);
-  printf("Input values (Data known): %d %d %d...%d %d %d\n", pedCorrected[0], pedCorrected[1], pedCorrected[2], pedCorrected[nPixels-3], pedCorrected[nPixels-2], pedCorrected[nPixels-1]);
-  printf("  max error: %e\n", maxError(a, pedCorrected, n));
+  printf("Input values (Data calc.): %8.2f %8.2f %8.2f...%8.2f %8.2f %8.2f\n", a[0], a[1], a[2], a[n-3], a[n-2], a[n-1]);
+  printf("Input values (Data known): %8.2f %8.2f %8.2f...%8.2f %8.2f %8.2f\n", pedCorrected[0], pedCorrected[1], pedCorrected[2], pedCorrected[nPixels-3], pedCorrected[nPixels-2], pedCorrected[nPixels-1]);
+  printf("  max error: %e\n", maxError(a, pedCorrected, nEvents, nPixels));
 
   // cleanup
   checkCuda( cudaEventDestroy(startEvent) );
